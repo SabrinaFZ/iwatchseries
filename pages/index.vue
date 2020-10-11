bad23b9f3f2bd90c08463ce2213df4346b2b330f
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2020-10-11 13:22:15 +0200

    Create Dropdown component to select serie

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,81 @@
         src="/logo.png"
       />
     </h1>
-    <InputComponent @input="searchSerie" />
+    <InputComponent @input="searchSerie" @clean-dropdown="cleanDropdown()" />
+    <DropdownComponent :items="seriesList" @add="addSerie" @remove="removeSelectedSerie" />
   </div>
 </template>
 
 <script>
 import InputComponent from '@/components/input/input.component';
+import DropdownComponent from '@/components/dropdown/dropdown.component';
 
 export default {
   components: {
-    InputComponent
+    InputComponent,
+    DropdownComponent
+  },
+  data() {
+    return {
+      seriesList: [],
+      selectedSeriesList: []
+    };
   },
   methods: {
-    searchSerie(serieTitle) {
-      // send a request to get serie info
-      console.log(serieTitle);
+    async searchSerie(serieTitle) {
+      const series = await this.$axios
+        .$get(`${process.env.tmbdBaseUrl}search/tv?api_key=${process.env.tmbdApiKey}&language=en-US&query=${serieTitle}`);
+      this.filterSeries(series.results.slice(0, 5));
+    },
+
+    filterSeries(seriesList) {
+      let filteredSeriesList = [];
+      const seriesListClone = [...seriesList];
+
+      this.selectedSeriesList.forEach(selectedSerie => {
+        seriesListClone.forEach((serie, index) => {
+          if (selectedSerie.id === serie.id) {
+            seriesListClone.splice(index, 1);
+            filteredSeriesList.push({
+              ...selectedSerie,
+              selected: true
+            });
+          }
+        });
+      });
+
+      filteredSeriesList = [
+        ...seriesListClone,
+        ...filteredSeriesList
+      ];
+
+      this.seriesList = filteredSeriesList;
+    },
+
+    cleanDropdown() {
+      this.seriesList = [];
+    },
+
+    addSerie(serie) {
+      this.selectedSeriesList.push(serie);
+      this.removeSerie(serie);
+    },
+
+    removeSerie(serie) {
+      this.seriesList = this.seriesList.filter(item => item.id !== serie.id);
+    },
+
+    removeSelectedSerie(serie) {
+      this.selectedSeriesList = this.selectedSeriesList.filter(item => item.id !== serie.id);
+      this.seriesList = this.seriesList.map(item => {
+        if (item.id === serie.id) {
+          return {
+            ...serie,
+            selected: false
+          };
+        }
+        return serie;
+      });
     }
   }
 };
